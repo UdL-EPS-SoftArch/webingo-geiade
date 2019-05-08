087eeeb160a70906e438d1de94ed551552b42ddd
Victor Ferrer <viicc6@aules-109-100.udl.net>
2019-05-08 15:41:15 +0200

    invitation to dropdawn added

---
+++ src/app/invitation/invitation-list/invitation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invitation',
+  templateUrl: './invitation.component.html',
+  styleUrls: ['./invitation-list.component.css']
+})
+export class InvitationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
