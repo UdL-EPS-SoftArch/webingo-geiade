9fffc4a932f8bd7c075b32bc00712e93f85d4014
Victor Ferrer <viicc6@aules-109-100.udl.net>
2019-05-14 18:27:08 +0200

    null invitation list done

--- src/app/invitation/invitation-list/invitation-list.component.ts
+++ src/app/invitation/invitation-list/invitation-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { InvitationService} from "../invitation.service";
+import {Invitation} from "../invitation";
+
 
 @Component({
-  selector: 'app-invitation',
+  selector: 'app-invitation-list',
   templateUrl: './invitation-list.component.html',
   styleUrls: ['./invitation-list.component.css']
 })
-export class InvitationListComponent implements OnInit {
 
-  constructor() { }
+export class InvitationListComponent implements OnInit {
+  public invitations: Invitation[] = [];
+  public totalInvitations = 0;
+
+  constructor(
+    public router: Router,
+    private invitationService: InvitationService) {
+  }
 
   ngOnInit() {
+    this.invitationService.getAll()
+      .subscribe(
+        invitations => {
+          this.invitations = invitations;
+          this.totalInvitations = this.invitations.length;
+        });
+  }
+
+  showSearchResults(invitations) {
+    this.invitations = invitations;
   }
 
 }
