99fc21cbf839133287d564e43f7666e2d82673d6
Genís Anaya Ibars <genisanaya@aules-109-192.udl.net>
2019-05-16 16:27:36 +0200

    Player ID and Game name available in Card Details

--- src/app/card/card-detail/card-detail.component.ts
+++ src/app/card/card-detail/card-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Card} from '../card';
+import {Game} from '../../game/game';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CardService} from '../card-service';
+import {GameService} from '../../game/game.service';
+import {Player} from '../../user/player';
 
 @Component({
   selector: 'app-card-detail',
@@ -13,13 +16,22 @@ export class CardDetailComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private cardService: CardService,
+              private gameService: GameService,
               private router: Router) {
   }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.cardService.get(id).subscribe(
-      card => this.card = card);
+      card => {
+        this.card = card;
+        this.card.getRelation(Game, 'game').subscribe(
+          game => this.card.game = game
+        );
+        this.card.getRelation(Player, 'player').subscribe(
+          player => this.card.player = player
+        );
+      });
   }
 
 }
