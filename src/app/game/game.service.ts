42f36d1ea59ae7abe1d7f3b55c962d9be2668cd4
danic96 <danic96@users.noreply.github.com>
2019-05-16 15:53:55 +0200

    join game working, but still not finished

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -2,16 +2,31 @@ import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { RestService } from 'angular4-hal-aot';
 import { Game } from './game';
+import {Card} from '../card/card';
+import {CardService} from '../card/card-service';
+import {Player} from "../user/player";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class GameService extends RestService<Game> {
+  private card: Card;
 
-  constructor(injector: Injector) {
+  constructor(injector: Injector,
+              private cardService: CardService,
+              private router: Router) {
     super(Game, 'games', injector);
   }
 
   public findByNameContaining(text: string): Observable<Game[]> {
     const options: any = {params: [{key: 'text', value: text}]};
     return this.search('findByNameContaining', options);
+  }
+
+  public joinGame(game: Game) {
+    this.card = new Card();
+    this.card.game = game;
+
+    this.cardService.create(this.card).subscribe(
+      (card: Card) => this.router.navigate([card.uri]));
   }
 }
