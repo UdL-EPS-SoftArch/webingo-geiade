70eabf888cd8aaf9ea15dcb9f635105f0056ab8b
davidcalavia <davidcalavia2@gmail.com>
2019-05-16 16:22:07 +0200

    delete inv

--- src/app/invitation/invitation-delete/invitation-delete.component.ts
+++ src/app/invitation/invitation-delete/invitation-delete.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { InvitationService} from "../invitation.service";
+import { Invitation} from "../invitation";
+
 
 @Component({
   selector: 'app-invitation-delete',
-  templateUrl: './invitation-delete.component.html',
-  styleUrls: ['./invitation-delete.component.css']
+  templateUrl: './game-invitation.component.html'
 })
 export class InvitationDeleteComponent implements OnInit {
+  public invitation: Invitation = new Invitation();
+  private id: string;
+  private type: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private invitationService: InvitationService) {
   }
 
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.invitationService.get(this.id).subscribe(
+      invitation => this.invitation = invitation);
+  }
+
+  delete() {
+    this.invitationService.delete(this.invitation).subscribe(
+      () => this.router.navigate(['invitation']));
+  }
 }
--- src/app/invitation/invitation-list/invitation-list.component.ts
+++ src/app/invitation/invitation-list/invitation-list.component.ts
@@ -7,7 +7,6 @@ import {Invitation} from "../invitation";
 @Component({
   selector: 'app-invitation-list',
   templateUrl: './invitation-list.component.html',
-  styleUrls: ['./invitation-list.component.css']
 })
 
 export class InvitationListComponent implements OnInit {
