ea95b5d8291b26099e369be69d66db1db4ad131f
davidcalavia <davidcalavia2@gmail.com>
2019-05-17 21:30:13 +0200

    modifications create delete detail

--- src/app/invitation/invitation-detail/invitation-detail.component.ts
+++ src/app/invitation/invitation-detail/invitation-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { InvitationService } from '../invitation.service';
 import { Invitation } from '../invitation';
+import { Player } from '../../user/player';
 
 @Component({
   selector: 'app-invitation-detail',
@@ -18,7 +19,15 @@ export class InvitationDetailComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.invitationService.get(id).subscribe(
-      invitation => this.invitation = invitation);
+      invitation => {
+        this.invitation = invitation;
+        this.invitation.getRelation(Player, 'playerInvited').subscribe(
+          playerInvited => this.invitation.playerInvited = playerInvited
+        );
+        this.invitation.getRelation(Player, 'playerWhoInvited').subscribe(
+          playerWhoInvited => this.invitation.playerWhoInvited = playerWhoInvited
+        );
+      });
   }
 
   public delete() {
